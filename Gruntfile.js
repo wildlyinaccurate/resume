79722d52a4211ca674bafa236c39e335b218fabd
Joseph Wynn <joseph@wildlyinaccurate.com>
2014-01-20 22:48:36 +0000

    Forgot CNAME and favicon for gh-pages...

--- Gruntfile.js
+++ Gruntfile.js
@@ -289,6 +289,8 @@ module.exports = function (grunt) {
         base: 'dist'
       },
       src: [
+        'CNAME',
+        'favicon.ico',
         'index.html',
         'images/*',
         'scripts/*',
