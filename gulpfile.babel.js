27965792266ef89b5ab361c7a33ac25cc86d4976
Joseph Wynn <joseph.wynn@bbc.co.uk>
2015-11-27 11:52:42 +0000

    Output to dist/app.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp')
 const webpack = require('gulp-webpack')
+const named = require('vinyl-named')
 
 gulp.task('default', () => {
   return gulp.src('src/app.js')
+    .pipe(named())
     .pipe(webpack(require('./webpack.config.js')))
     .pipe(gulp.dest('dist'))
 })
