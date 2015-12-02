dbfa76114a61e08671697052a1fe099934699a86
Joseph Wynn <joseph.wynn@bbc.co.uk>
2015-12-02 22:13:06 +0000

    Aw, that looks nice

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Skills extends Component {
   render() {
     return (
-      <div className="skills text-center">
+      <div id="skills" className="text-center">
         <h2 className="display-1 m-b-md text-center">Skills</h2>
       </div>
     )
