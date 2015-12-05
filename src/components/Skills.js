577532961e1873831ba78bd79a9d3dd74b5bbe85
Joseph Wynn <joseph@wildlyinaccurate.com>
2015-12-05 21:21:17 +0000

    Skills section filling out

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
+import SkillItem from './SkillItem'
 
-class Skills extends Component {
-  render() {
+const Skills = React.createClass({
+  getInitialState: function() {
+    return {
+      skills: ''
+    }
+  },
+
+  componentDidMount: function() {
+    $.getJSON('data/skills.json')
+      .then(this.dataToSkillItems)
+      .then((skills) => {
+        this.setState({ skills })
+      })
+  },
+
+  dataToSkillItems: function(data) {
+    return data.results.map((props) => {
+      return <SkillItem {...props} />
+    })
+  },
+
+  render: function() {
     return (
       <div id="skills" className="text-center">
         <h2 className="display-1 m-b-md text-center">Skills</h2>
+
+        <div className="row">
+          {this.state.skills}
+        </div>
       </div>
     )
   }
-}
+})
 
 export default Skills
