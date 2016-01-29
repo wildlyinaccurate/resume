73f0483bda8243153d783f47767a78cb48c51435
Joseph Wynn <joseph@wildlyinaccurate.com>
2016-01-29 12:55:59 +0000

    Spellcheck

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -6,11 +6,11 @@ class Introduction extends Component {
       <div id="introduction" className="introduction section text-xs-center">
         <h4 className="introduction__heading display-2">Hello!</h4>
         <p className="lead">
-            I'm a developer based in London, UK. Right now I am working as a JavaScript, Ruby and PHP developer; but in my spare time I like to write Haskell and Elm. I'm really passionate about building a better web, and I try to do my part by evangelising good development practices and modern web technologies.
+          I'm a developer based in London, UK. Right now I am working as a JavaScript, Ruby and PHP developer; but in my spare time I like to write Haskell and Elm. I'm really passionate about building a better web, and I try to do my part by evangelising good development practices and modern web technologies.
         </p>
 
         <p className="lead">
-            Outside of programming I am a cook, runner, rock climber and guitarist. I'm also an <a href="https://wildlyinaccurate.com/accidental-keyboard-enthusiasm/">accidental keyboard enthuiast</a>. I sometimes blog at <a href="https://wildlyinaccurate.com/">Wildly Inaccurate</a>.
+          Outside of programming I am a cook, runner, rock climber and guitarist. I'm also an <a href="https://wildlyinaccurate.com/accidental-keyboard-enthusiasm/">accidental keyboard enthusiast</a>. I sometimes blog at <a href="https://wildlyinaccurate.com/">Wildly Inaccurate</a>.
         </p>
       </div>
     )
