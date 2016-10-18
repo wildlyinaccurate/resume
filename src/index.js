cb5b7ab83d6108e25811a6b022d5ded30fbe87d2
Joseph Wynn <joseph@wildlyinaccurate.com>
2016-10-18 22:43:46 +0100

    Use the third argument of render() to replace the statically-rendered node

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ const { h, render } = require('preact')
 const App = require('./containers/app')
 
 const container = document.getElementById('app-container')
-const staticMarkup = container.childNodes[0]
 
-render(<App />, container)
-
-container.removeChild(staticMarkup)
+render(<App />, container.parentNode, container)
