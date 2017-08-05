ca7189ad5ebff0cd05f79e757f4ba967b7e49e9a
Joseph Wynn <joseph@wildlyinaccurate.com>
2017-08-05 19:51:11 +0100

    "Full stack" lol

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -5,7 +5,7 @@ module.exports = function Introduction () {
     <div id="introduction" className="introduction section text-center px-0 py-4 px-md-5">
       <h5 className="introduction__heading display-3">Hello!</h5>
       <p className="lead">
-        My name is Joseph Wynn. I'm a web developer with a focus on performance and accessibility. My experience is with high-traffic, high-availability distributed systems at companies like BBC News and Time. I maintain some <a href="https://github.com/wildlyinaccurate/">open source projects</a><span className="visible-print-inline"> (github.com/wildlyinaccurate)</span> and occasionally <a href="https://wildlyinaccurate.com/">blog about my experiences</a><span className="visible-print-inline"> (wildlyinaccurate.com)</span>.
+        My name is Joseph Wynn. I'm a full stack web developer with a focus on performance and accessibility. My experience is with high-traffic, high-availability distributed systems at companies like BBC News and Time. I maintain some <a href="https://github.com/wildlyinaccurate/">open source projects</a><span className="visible-print-inline"> (github.com/wildlyinaccurate)</span> and occasionally <a href="https://wildlyinaccurate.com/">blog about my experiences</a><span className="visible-print-inline"> (wildlyinaccurate.com)</span>.
       </p>
     </div>
   )
